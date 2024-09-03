40bf2ddc191e1e49ae628f4caa7e979d738fc2b9
Edgar Flores <edgarhflores@gmail.com>
2024-09-03 09:51:20 -0500

    code clean up and working on icons

---
+++ project-interview-task/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("fa-regular-400.otf", Alias = "FAR")]
+[assembly: ExportFont("fa-solid-900.otf", Alias = "FAS")]
+[assembly: ExportFont("fa-brands-400.otf", Alias = "FAB")]
